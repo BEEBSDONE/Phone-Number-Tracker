902e3513fdd9f422c616ddec0b3c67c9b6573615
DEDA <beebsdone@gmail.com>
2022-05-23 22:21:07 +0200

    initiation

---
+++ pnumtracker.py
@@ -0,0 +1,28 @@
+import phonenumbers
+from phonenumbers import carrier
+from phonenumbers import geocoder
+from phonenumbers import timezone
+from timezonefinder import TimezoneFinder
+from geopy.geocoders import Nominatim
+
+target = "+905322955612"
+
+def track():
+    number = phonenumbers.parse(target)
+    locate = geocoder.description_for_number(number, 'en')
+    print(locate)
+
+    operator = carrier.name_for_number(number, 'en')
+    print(operator)
+
+    time = timezone.time_zones_for_number(number)
+    print(time)
+
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.geocode(locate)
+    lng = location.longitude
+    lat = location.latitude
+    print("Longitude:", lng)
+    print("Latitude:", lat)
+
+track()
